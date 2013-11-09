1fd144b8f43c6a91100db96872c58ace315a563c
manatails <admin@manateeshome.com>
2013-11-09 09:49:22 +0900

    Populating repo

---
+++ plugin/manateebans/lua/manateebans_config.lua
@@ -0,0 +1,13 @@
+--[[
+
+ManateeBans Configuration File
+
+]]--
+
+manateedb_host = "yourhostname"
+manateedb_user = "yourusername"
+manateedb_pass = "yourpassword"
+manateedb_name = "yourdatabase"
+manateedb_port = 3306
+servername = "Short Server Name" -- ex) TTT #1, Deathrun #1
+manateebans_serverid = "ID" -- ex) TTT1, DR1

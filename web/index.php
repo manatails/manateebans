912e5d88432902bb096cc45e602a766393610bda
manatails <admin@manateeshome.com>
2013-11-09 09:43:03 +0900

    Creating directory structure

---
+++ web/index.php
@@ -0,0 +1,7 @@
+<html>
+<title>매너티밴 메인 메뉴</title>
+
+<h2 align="center">매너티밴</h2>
+<p align="center"><a href="search_steamid.php">밴 검색</a></p>
+<p align="center"><a href="admin/">어드민 메뉴</a></p>
+</html>

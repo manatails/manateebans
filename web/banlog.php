0adfdb237c14643088f9f22b8d430bb7fec23da6
manatails <admin@manateeshome.com>
2013-11-17 23:07:30 +0900

    Banlog viewer, minor fix

---
+++ web/banlog.php
@@ -0,0 +1,55 @@
+﻿<html>
+<head>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="manatee.css">
+</head>
+<title>매너티밴 차단 목록</title>
+
+<?php
+
+include 'config.php';
+
+// Connecting, selecting database
+$link = mysql_connect($mana_host, $mana_user, $mana_pass)
+    or die('Could not connect: ' . mysql_error());
+mysql_select_db($mana_table) or die('Could not select database');
+
+// Performing SQL query
+
+mysql_query("SET NAMES latin1");
+
+$query = 'SELECT * FROM manateebans_log ORDER BY time DESC LIMIT 20';
+$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+
+// Printing results in HTML
+  
+echo "<div class=\"datagrid\"><table>\n";
+echo "  <thead><tr><th>차단 목록</th></tr></thead>
+        <tr><td>고유번호</td>
+		<td>아이피</td>
+		<td>닉네임</td>
+		<td>시간</td>
+		<td>서버</td></tr>";
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $k => $col_value) {
+	
+	    if ($k == 'time')
+	        $col_value = gmdate("Y-m-d H:i:s", $col_value);
+				
+        echo "\t\t<td>$col_value</td>\n";
+    
+	}
+    echo "\t</tr>\n";
+}
+echo "</table></div>\n";
+
+// Free resultset
+mysql_free_result($result);
+
+// Closing connection
+mysql_close($link);
+?>
+    <br>
+    <div align="right"><a href="index.php">메인 메뉴</a></div>
+</html>
--- web/index.php
+++ web/index.php
@@ -1,7 +1,17 @@
 <html>
+<head>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="manatee.css">
+</head>
 <title>매너티밴 메인 메뉴</title>
 
-<h2 align="center">매너티밴</h2>
+<div align="center">
+  <h2>매너티밴</h2>
+</div>
+
 <p align="center"><a href="search_steamid.php">밴 검색</a></p>
+<p align="center"><a href="search_ip.php">밴 IP 검색</a></p>
+<p align="center"><a href="list.php">최근 밴 기록</a></p>
+<p align="center"><a href="banlog.php">최근 차단 기록</a></p>
 <p align="center"><a href="admin/">어드민 메뉴</a></p>
 </html>
